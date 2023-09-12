3264db3646177765572fbaabff7a220e921d34da
JuanTasiguano <142605220+JuanTasiguano@users.noreply.github.com>
2023-09-12 08:51:35 -0500

    Juan Tasiguabi

---
+++ JuanTasiguano/JuanTasiguano/UnitTest1.cs
@@ -0,0 +1,21 @@
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace JuanTasiguano
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
